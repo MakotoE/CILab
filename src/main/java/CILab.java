95d1168e1c91cabfb6902297b7be244e368e8c81
Makoto <makoto@emurasoft.com>
2021-06-11 14:14:22 -0700

    detectCapitalUse()

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -1,20 +1,43 @@
 public class CILab implements CILabInterface {
-    private String myString;
+	private String myString;
 
-    @Override
-    public String getString() {
-        return myString;
-    }
+	@Override
+	public String getString() {
+		return myString;
+	}
 
-    @Override
-    public void setString(String string) {
-        myString = string;
-    }
+	@Override
+	public void setString(String string) {
+		myString = string;
+	}
 
-    @Override
-    public boolean detectCapitalUse() {
-        return false;
-    }
+	@Override
+	public boolean detectCapitalUse() {
+		if (myString.isEmpty()) {
+			return true;
+		}
+
+		for (var word : myString.split("\\s+")) {
+			if (!(word.codePoints().allMatch(Character::isUpperCase) || word.codePoints().allMatch(
+				Character::isLowerCase))) {
+				if (word.length() == 0) {
+					continue;
+				}
+
+				var characters = word.codePoints().toArray();
+				if (!Character.isUpperCase(characters[0])) {
+					return false;
+				}
+
+				for (int i = 1; i < characters.length; ++i) {
+					if (Character.isUpperCase(characters[i])) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
 
 }
 
--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -20,13 +20,61 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+       myString.setString("");
+       assertTrue(myString.detectCapitalUse());
     }
+
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("a");
+        assertTrue(myString.detectCapitalUse());
     }
 
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString(" a");
+        assertTrue(myString.detectCapitalUse());
+    }
 
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString(" A");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("a ");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest6() {
+        myString.setString("aA");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest7() {
+        myString.setString("Aa");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest8() {
+        myString.setString("USA");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest9() {
+        myString.setString("USa");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest10() {
+        myString.setString("Google");
+        assertTrue(myString.detectCapitalUse());
+    }
 }
